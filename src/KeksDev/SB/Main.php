21ff4f189cb5ad84c7586c660d9013dae304c1f9
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2021-04-13 10:17:51 +0200

    Create Main.php

---
+++ src/KeksDev/SB/Main.php
@@ -0,0 +1,52 @@
+<?php
+namespace KeksDev\SB;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
+
+class Main extends PluginBase {
+  public function setScoreboardEntry(Player $player, int $score, string $msg, string $objName)
+    {
+        $entry = new ScorePacketEntry();
+        $entry->objectiveName = $objName;
+        $entry->type = 3;
+        $entry->customName = " $msg   ";
+        $entry->score = $score;
+        $entry->scoreboardId = $score;
+        $pk = new SetScorePacket();
+        $pk->type = 0;
+        $pk->entries[$score] = $entry;
+        $player->sendDataPacket($pk);
+    }
+
+    public function rmScoreboardEntry(Player $player, int $score)
+    {
+        $pk = new SetScorePacket();
+        if (isset($pk->entries[$score])) {
+            unset($pk->entries[$score]);
+            $player->sendDataPacket($pk);
+        }
+    }
+
+    public function createScoreboard(Player $player, string $title, string $objName, string $slot = "sidebar", $order = 0)
+    {
+        $pk = new SetDisplayObjectivePacket();
+        $pk->displaySlot = $slot;
+        $pk->objectiveName = $objName;
+        $pk->displayName = $title;
+        $pk->criteriaName = "dummy";
+        $pk->sortOrder = $order;
+        $player->sendDataPacket($pk);
+    }
+
+    public function rmScoreboard(Player $player, string $objName)
+    {
+        $pk = new RemoveObjectivePacket();
+        $pk->objectiveName = $objName;
+        $player->sendDataPacket($pk);
+    }
+}
