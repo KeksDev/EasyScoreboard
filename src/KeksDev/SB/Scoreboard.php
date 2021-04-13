a4c1367f4170c4b2b6ffebce80cd1667f73d019f
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2021-04-13 10:20:59 +0200

    Create Scoreboard.php

---
+++ src/KeksDev/SB/Scoreboard.php
@@ -0,0 +1,23 @@
+<?php
+namespace KeksDev\SB;
+
+class Scoreboard {
+ public function __construct(Player $p, String $name, $data = []) {
+        $this->name = $name
+        $this->p = $p;
+        $this->data = $data;
+    }
+
+    public function send() {
+        $player = $this->p;
+        Main::rmScoreboard($player, $this->name);
+        Main::createScoreboard($player, $this->name, $this->name);
+
+        $count = 0;
+
+        foreach($this->data as $d) {
+            $sc->setScoreboardEntry($player, $count, $d, $this->name);
+            $count++;
+        }
+    } 
+}
